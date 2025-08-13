d0ffb7c5f03c205012f9f118746a5a25859d427c
yeahChibyke <chibykepride@gmail.com>
2025-08-13 03:32:31 +0100

    getting the hang of vyper fuzzing

--- tests/fuzz/fuzz_token.py
+++ tests/fuzz/fuzz_token.py
@@ -1,16 +1,32 @@
 import boa
 from boa.test.strategies import strategy
-from hypothesis import HealthCheck, given, settings
+from hypothesis import settings
+from hypothesis.stateful import RuleBasedStateMachine, rule
+
+from src import mox_token
 
 
-@settings(
-    max_examples=1000, suppress_health_check=[HealthCheck.function_scoped_fixture]
-)
-@given(new_max_supply=strategy("uint256"))
-def test_should_not_be_able_to_increase_max_supply_to_current_max_supply(
-    mox, owner, new_max_supply
-):
-    print(new_max_supply)
-    with boa.env.prank(owner):
-        mox.increase_max_supply(new_max_supply)
-    # assert mox.get_max_supply() == new_max_supply
+class MaxSupplyIncreaseAndDecreaseFuzzer(RuleBasedStateMachine):
+    def __init__(self):
+        super().__init__()
+        self.contract = mox_token.deploy()
+        print("Contract deployed!!!")
+
+    @rule(new_max_supply = strategy("uint256"))
+    def increase_max_supply(self, new_max_supply):
+        with boa.env.prank(self.contract.owner):
+            self.contract.increase_max_supply(new_max_supply)
+            response = self.contract.get_max_supply()
+            assert response == new_max_supply, f"Expected {new_max_supply}, got {response}"
+            print(f"called increase_max_supply with {new_max_supply}")
+
+    @rule(new_max_supply = strategy("uint256"))
+    def decrease_max_supply(self, new_max_supply):
+        with boa.env.prank(self.contract.owner):
+            self.contract.decrease_max_supply(new_max_supply)
+            response = self.contract.get_max_supply()
+            assert response == new_max_supply, f"Expected {new_max_supply}, got {response}"
+            print(f"Called decrease_max-supply with {new_max_supply}")
+
+TokenFuzzing = MaxSupplyIncreaseAndDecreaseFuzzer.TestCase
+TokenFuzzing.settings = settings(max_examples=1000, stateful_step_count=50)
